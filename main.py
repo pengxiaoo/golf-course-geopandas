e0acd6aef791de9287e5878cd4bf623ad04de94b
pengxiaoo <alexpengxiao@gmail.com>
2024-12-21 19:57:38 +0800

    use icon as markers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from shapely.geometry import Polygon, Point, LineString
 from scipy.ndimage import gaussian_filter1d
 
@@ -31,18 +32,18 @@ item_colors = {
     "Approach": "white",
     "Tee": "white",
 }
-# todo: replace markers with icons
-item_markers = {
-    "LeafyTree": "^",
-    "Green": "o",
-    "Approach": "o",
-    "Tee": "o",
+item_marker_icon_paths = {
+    # todo: ask designer to provide better icons
+    "LeafyTree": "icons/leafy_tree.png",
+    "Green": "icons/green.webp",
+    "Approach": "icons/approach.png",
+    "Tee": "icons/tee.png",
 }
 marker_sizes = {
-    "LeafyTree": 100,
-    "Green": 20,
-    "Approach": 20,
-    "Tee": 20,
+    "LeafyTree": 200,
+    "Green": 100,
+    "Approach": 10,
+    "Tee": 100,
 }
 line_widths = {
     "WaterPath": 2.0,
@@ -62,15 +63,15 @@ def get_smooth_polygon(coords):
     return Polygon(smoothed_coords)
 
 
-def plot_markers(ax, points, boundary, item_type):
-    x, y = [], []
+def plot_markers_with_icons(ax, points, hole_boundary, item_type):
     for point in points:
-        point_obj = Point(point)
-        if boundary.contains(point_obj):
-            x.append(point[0])
-            y.append(point[1])
-    ax.scatter(x, y, color=item_colors[item_type], marker=item_markers[item_type], s=marker_sizes[item_type],
-               label=item_type)
+        if inside_polygon(point, hole_boundary):
+            icon_path = item_marker_icon_paths[item_type]
+            icon = plt.imread(icon_path)
+            zoom_factor = (marker_sizes[item_type] / 100) * 0.05
+            imagebox = OffsetImage(icon, zoom=zoom_factor)
+            ab = AnnotationBbox(imagebox, (point[0], point[1]), frameon=False, pad=0)
+            ax.add_artist(ab)
 
 
 def inside_polygon(coord, polygon):
@@ -142,10 +143,10 @@ def plot_golf_course(json_file_path, hole_number):
             gpd.GeoSeries([row.geometry]).plot(ax=ax, color=row['color'], edgecolor=edge_color, linewidth=default_width)
     green_trace = gdf[gdf['itemType'] == "GreenTrace"]
     green_trace.plot(ax=ax, color=green_trace['color'], edgecolor=edge_color, linewidth=default_width)
-    plot_markers(ax, leafy_tree_points, hole_boundary, "LeafyTree")
-    plot_markers(ax, [green_coord], hole_boundary, "Green")
-    plot_markers(ax, [approach_coord], hole_boundary, "Approach")
-    plot_markers(ax, [tee_coord], hole_boundary, "Tee")
+    plot_markers_with_icons(ax, leafy_tree_points, hole_boundary, "LeafyTree")
+    plot_markers_with_icons(ax, [green_coord], hole_boundary, "Green")
+    plot_markers_with_icons(ax, [approach_coord], hole_boundary, "Approach")
+    plot_markers_with_icons(ax, [tee_coord], hole_boundary, "Tee")
     ax.set_title(f"Golf Course Hole Layout (hole {hole_number})")
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
